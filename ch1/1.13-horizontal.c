55bf7c16b24e6245765b4fa74426a62fb0597033
Maria Pacana <maria.pacana@gmail.com>
2015-08-16 11:44:39 -0700

    Finished word-counting horizontal histogram

---
+++ ch1/1.13-horizontal.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#define OUT 0 /*we are outside a word*/
+#define IN  1 /* we are in a word */
+
+main()
+{
+	int c;
+	int i, j;
+	int length = 0;
+	int state = OUT; /*are we in a word or not?*/
+	int wordlength[10];
+
+	/* Initialize wordlength array. */
+	for (i = 0; i < 10; ++i)
+		wordlength[i] = 0;
+
+	/* Store word lengths */
+	while ((c = getchar()) != EOF)
+	{
+		if (c == ' ' || c == '\n' || c == '\t')
+		{
+			if (state == IN) {
+				state = OUT;
+				++wordlength[length];
+				length = 0;
+			}
+		}
+		else
+		{
+			if (state == OUT) {
+				state = IN;
+			}
+			length++;
+		}
+	}
+
+	printf("Horizontal Histogram:\n");
+
+	/* Print the histogram */
+	for (i = 0; i < 10; ++i) {
+		for (j = 0; j < wordlength[i]; ++j) {
+			putchar('*');
+		}
+		putchar('\n');
+	}
+}
