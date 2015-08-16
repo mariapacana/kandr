ab7d5af5413c5e712e21176d62db0e44168dcc2f
Maria Pacana <maria.pacana@gmail.com>
2015-08-16 13:06:35 -0700

    Use constant MAXLEN

--- ch1/1.13-horizontal.c
+++ ch1/1.13-horizontal.c
@@ -2,6 +2,7 @@
 
 #define OUT 0 /*we are outside a word*/
 #define IN  1 /* we are in a word */
+#define MAXLEN 10 /*max length of a word*/
 
 main()
 {
@@ -9,10 +10,10 @@ main()
 	int i, j;
 	int length = 0;
 	int state = OUT; /*are we in a word or not?*/
-	int wordlength[10];
+	int wordlength[MAXLEN];
 
 	/* Initialize wordlength array. */
-	for (i = 0; i < 10; ++i)
+	for (i = 0; i < MAXLEN; ++i)
 		wordlength[i] = 0;
 
 	/* Store word lengths */
@@ -38,7 +39,7 @@ main()
 	printf("Horizontal Histogram:\n");
 
 	/* Print the histogram */
-	for (i = 0; i < 10; ++i) {
+	for (i = 0; i < MAXLEN; ++i) {
 		for (j = 0; j < wordlength[i]; ++j) {
 			putchar('*');
 		}
